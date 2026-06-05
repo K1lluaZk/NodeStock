ecc02702804d69f598c000975072b1bba5382471
Mario Suero <mario.suerodejesus@gmail.com>
2026-06-04 20:29:32 -0400

    solucionando vulnerabilidades

--- server/config.js
+++ server/config.js
@@ -4,5 +4,6 @@ dotenv.config()
 export const {
   PORT = 3000,
   SALT_ROUNDS = 10,
-  SECRET_JWT_KEY = 'K1lluaZk_JWT_9fA3xQ7mP2LwD8RkS5VbE0HnC4YJ'
+  SECRET_JWT_KEY,
+  REFRESH_SECRET_KEY
 } = process.env
